859e97e9763e0ecae1c7c3a3494189416d5b63d0
jimchen <jimchen@sustaihub.com>
2025-01-01 19:24:45 +0800

    fix: refactor navbar to simplify UI elements
    - Remove "Pricing" link from the navbar
    
    Signed-off-by: jimchen <jimchen@sustaihub.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -39,15 +39,6 @@ export const Navbar = async () => {
             ) : (
               <>
                 <Link
-                  href="/dashboard"
-                  className={buttonVariants({
-                    size: "sm",
-                    variant: "ghost",
-                  })}
-                >
-                  Pricing
-                </Link>
-                <Link
                   href="/sign-in"
                   className={buttonVariants({
                     size: "sm",
